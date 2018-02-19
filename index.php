4d711478dd218785618a14b04aa9915905fd2284
bunian <admin@bunian.cn>
2018-02-19 10:11:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,12 +49,12 @@ function curl($url,$post=0,$header=0,$cookie=0,$referer=0,$ua=0,$nobaody=0){
 $about=['伤感','孤独','失恋','伤心','搞笑'];
 $pn=mt_rand(0,4);
 $text=curl('http://api.bunian.cn/yulu/?fun=wb&about='.$about[$pn]);
-
+$pic_id='';
 $post=[
 'title' =>'今日要说什么？',
 'location' => 'v6_content_home',
 'text' => $text,//需要发送微博的内容
-'pic_id' => @$pic_id,//微博图片id，需事先上传好
+'pic_id' => $pic_id,//微博图片id，需事先上传好
 'isReEdit' => false,
 'pub_source' => 'page_2',
 'topic_id' => '1022%3A',
@@ -64,6 +64,6 @@ $post=[
 ];
 $url='https://weibo.com/aj/mblog/add?ajwvr=6&__rnd=2918942797035';//不需要改变
 $referer='https://weibo.com/u/5458778095/home?topnav=1&wvr=6';//可改可不改
-$cookie='UOR=qsalg.com,widget.weibo.com,yule.iqiyi.com; ALF=1521510026; SUB=_2A253jKnaDeRhGeNK7loW9ybMwjmIHXVUjjeSrDV8PUJbkNANLXekkW1NSUziFmL8PDSB0c4ESYbonLL9hFkeqmwx; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5gNxF.blb8vKn8UIbfkg1o5JpX5oz75NHD95QfSh-RS0MReh.fWs4Dqcj_i--ciK.Ni-27i--Xi-iWi-iWi--NiKLWiKnXi--fi-88iKn4i--ciKnfi-z7; YF-Ugrow-G0=9642b0b34b4c0d569ed7a372f8823a8e; wvr=6; YF-V5-G0=9717632f62066ddd544bf04f733ad50a; YF-Page-G0=074bd03ae4e08433ef66c71c2777fd84; _s_tentry=-; Apache=9440956109980.855.1518942248576; SINAGLOBAL=9440956109980.855.1518942248576; ULV=1518942248655:1:1:1:9440956109980.855.1518942248576:';//微博 Cookies
+$cookie='';//微博 Cookies
 curl($url,$post,'',$cookie,$referer);
 echo true;
